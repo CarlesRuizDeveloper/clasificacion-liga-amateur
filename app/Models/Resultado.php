707fb792132ac98c478790d779ce19907e1dc741
CarlesRuizDeveloper <crmfeina@gmail.com>
2024-10-25 12:36:01 +0200

    Crear modelos de Equipo y Resultado con relaciones definidas

---
+++ app/Models/Resultado.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Resultado extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'equipo_local_id',
+        'equipo_visitante_id',
+        'goles_local',
+        'goles_visitante',
+        'fecha',
+        'hora'
+    ];
+
+    public function equipoLocal()
+    {
+        return $this->belongsTo(Equipo::class, 'equipo_local_id');
+    }
+
+    public function equipoVisitante()
+    {
+        return $this->belongsTo(Equipo::class, 'equipo_visitante_id');
+    }
+}
