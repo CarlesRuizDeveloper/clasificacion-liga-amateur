abe039252ee2d669f0509c0e3a0b82f0670e72b9
CarlesRuizDeveloper <crmfeina@gmail.com>
2024-11-04 13:28:59 +0100

    Creado modelo partido y sus relaciones con equipo y resultado. Tambien las migraciones de partido

---
+++ app/Models/Partido.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Partido extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'equipo_local_id',
+        'equipo_visitante_id',
+        'jornada',
+        'fecha',
+        'hora'
+    ];
+
+    public function equipoLocal()
+    {
+        return $this->belongsTo(Equipo::class, 'equipo_local_id');
+    }
+
+    public function equipoVisitante()
+    {
+        return $this->belongsTo(Equipo::class, 'equipo_visitante_id');
+    }
+
+    public function resultado()
+    {
+        return $this->hasOne(Resultado::class);
+    }
+}
